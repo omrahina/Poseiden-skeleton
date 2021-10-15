5971daea5c7e14532081de4c90ca69370c797131
omrahina <crave627@gmail.com>
2021-10-15 13:22:36 +0200

    CRUD for Rating

--- src/main/java/com/nnk/springboot/controllers/RatingController.java
+++ src/main/java/com/nnk/springboot/controllers/RatingController.java
@@ -1,6 +1,9 @@
 package com.nnk.springboot.controllers;
 
 import com.nnk.springboot.domain.Rating;
+import com.nnk.springboot.service.RatingService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -10,32 +13,52 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @Controller
+@Slf4j
 public class RatingController {
     // TODO: Inject Rating service
+
+    @Autowired
+    private RatingService ratingService;
 
     @RequestMapping("/rating/list")
     public String home(Model model)
     {
         // TODO: find all Rating, add to model
+        List<Rating> ratings = ratingService.getAllRating();
+        if(!ratings.isEmpty()){
+            model.addAttribute("ratings", ratings);
+        }
+        model.addAttribute("rating", new Rating());
         return "rating/list";
     }
 
     @GetMapping("/rating/add")
     public String addRatingForm(Rating rating) {
+        log.info("Request add a new rating");
         return "rating/add";
     }
 
     @PostMapping("/rating/validate")
     public String validate(@Valid Rating rating, BindingResult result, Model model) {
         // TODO: check data valid and save to db, after saving return Rating list
-        return "rating/add";
+        if (result.hasErrors()){
+            log.error("Error(s) in the form");
+            return "rating/add";
+        }
+        ratingService.addRating(rating);
+        return "redirect:/rating/list";
     }
 
     @GetMapping("/rating/update/{id}")
     public String showUpdateForm(@PathVariable("id") Integer id, Model model) {
         // TODO: get Rating by Id and to model then show to the form
+        Rating rating = ratingService.getRating(id);
+        if (rating != null){
+            model.addAttribute("rating", rating);
+        }
         return "rating/update";
     }
 
@@ -43,12 +66,20 @@ public class RatingController {
     public String updateRating(@PathVariable("id") Integer id, @Valid Rating rating,
                              BindingResult result, Model model) {
         // TODO: check required fields, if valid call service to update Rating and return Rating list
+        if (result.hasErrors()){
+            log.error("Error(s) in the form");
+            model.addAttribute("rating", rating);
+            return "rating/update";
+        }
+        ratingService.updateRating(rating);
         return "redirect:/rating/list";
     }
 
     @GetMapping("/rating/delete/{id}")
     public String deleteRating(@PathVariable("id") Integer id, Model model) {
         // TODO: Find Rating by Id and delete the Rating, return to Rating list
+        log.info("Request delete rating");
+        ratingService.deleteRating(id);
         return "redirect:/rating/list";
     }
 }
---
+++ src/main/java/com/nnk/springboot/service/RatingService.java
@@ -0,0 +1,73 @@
+package com.nnk.springboot.service;
+
+import com.nnk.springboot.domain.Rating;
+import com.nnk.springboot.repositories.RatingRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Slf4j
+public class RatingService {
+
+    @Autowired
+    private RatingRepository ratingRepository;
+
+    public List<Rating> getAllRating(){
+        List<Rating> ratings = ratingRepository.findAll();
+        if (!ratings.isEmpty()){
+            log.info("Rating(s) found");
+            return ratings;
+        }
+        log.error("No Rating found.");
+        return List.of();
+    }
+
+    public Rating addRating(Rating rating){
+        try{
+            Rating savedRating = ratingRepository.save(rating);
+            log.info("Rating successfully saved.");
+            return savedRating;
+        } catch (Exception e){
+            log.error(e.getMessage());
+            return null;
+        }
+    }
+
+    public Rating updateRating(Rating rating){
+        Rating ratingToUpdate = ratingRepository.findRatingById(rating.getId());
+        if (ratingToUpdate != null){
+            log.info("Update in progress ...");
+            ratingToUpdate.setMoodysRating(rating.getMoodysRating());
+            ratingToUpdate.setSandPRating(rating.getSandPRating());
+            ratingToUpdate.setFitchRating(ratingToUpdate.getFitchRating());
+            ratingToUpdate.setOrderNumber(rating.getOrderNumber());
+            return ratingRepository.save(ratingToUpdate);
+        }
+        log.error("Unable to update the rating.");
+        return null;
+    }
+
+    public void deleteRating(int id){
+        Rating rating = ratingRepository.findRatingById(id);
+        if (rating != null){
+            ratingRepository.delete(rating);
+        } else {
+            log.error("Deletion failure");
+        }
+
+        log.info("Rating deleted");
+    }
+
+    public Rating getRating(Integer id) {
+        Rating rating = ratingRepository.findRatingById(id);
+        if (rating != null){
+            log.info("Rating found");
+            return rating;
+        }
+        log.error("No Rating found.");
+        return null;
+    }
+}
